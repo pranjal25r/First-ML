87906dc5f5ee39a4c70d3b7ca9a0824587003bf4
pranjal25r <pranjal25r@gmail.com>
2026-02-07 12:26:20 +0530

    updates in comparision file

--- Model Comparison.py
+++ Model Comparison.py
@@ -1,6 +1,54 @@
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
+from sklearn.metrics import mean_squared_error, r2_score
+
+'''Loading Data and Preprocessing'''
+df = pd.read_csv('delaney_solubility_with_descriptors.csv')  
+
+y = df['logS']  
+X = df.drop('logS', axis = 1)
+
+from sklearn.model_selection import train_test_split 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=100)
+
+'''Linear Regression Function'''
+from sklearn.linear_model import LinearRegression
+
+lr = LinearRegression()
+lr.fit(X_train,y_train)
+
+y_lr_train_pred = lr.predict(X_train)
+y_lr_test_pred = lr.predict(X_test)
+
+lr_train_mse = mean_squared_error(y_train,y_lr_train_pred)
+lr_train_r2 = r2_score(y_train,y_lr_train_pred)
+
+lr_test_mse = mean_squared_error(y_test,y_lr_test_pred)
+lr_test_r2 = r2_score(y_test,y_lr_test_pred)
+
+lr_results = pd.DataFrame(['Linear Regression', lr_train_mse, lr_train_r2, lr_test_mse, lr_test_r2]).transpose()
+lr_results.columns = ['Method', 'Training MSE', 'Training R2', 'Test MSE', 'Test R2']
+
+
+'''Random Forest Function'''
+from sklearn.ensemble import RandomForestRegressor
+
+rf = RandomForestRegressor(max_depth=2, random_state=100)
+rf.fit(X_train,y_train)
+
+y_rf_train_pred = rf.predict(X_train)
+y_rf_test_pred = rf.predict(X_test)
+
+rf_train_mse = mean_squared_error(y_train,y_rf_train_pred)
+rf_train_r2 = r2_score(y_train,y_rf_train_pred)
+
+rf_test_mse = mean_squared_error(y_test,y_rf_test_pred)
+rf_test_r2 = r2_score(y_test,y_rf_test_pred)
+
+rf_results = pd.DataFrame(['Random Forest ', rf_train_mse, rf_train_r2, rf_test_mse, rf_test_r2]).transpose()
+rf_results.columns = ['Method', 'Training MSE', 'Training R2', 'Test MSE', 'Test R2']
+
 
 df_models = pd.concat([lr_results, rf_results], axis=0).reset_index(drop=True)
 print(df_models)
